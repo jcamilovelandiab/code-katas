e4334d867537ef52d6fa4b7c5b5895ab440f3439
Juan Camilo Velandia Botello <jcamilovelandiab@gmail.com>
2025-05-27 10:01:45 -0400

    chore: Add scaffolding and input files for exercise 9 advent of code 2015

---
+++ advent_of_code_2015/src/ex9_2015/ex9.rs
@@ -0,0 +1,6 @@
+pub fn run_ex9_2015() {
+    let input = std::fs::read_to_string("src/ex9_2015/input.txt").unwrap();
+    for line in input.lines() {
+        println!("{}", line);
+    }
+}
--- advent_of_code_2015/src/main.rs
+++ advent_of_code_2015/src/main.rs
@@ -1,7 +1,10 @@
 use ex8_2015::ex8::run_ex8_2015;
+use ex9_2015::ex9::run_ex9_2015;
 
 mod ex8_2015;
+mod ex9_2015;
 
 fn main() {
     run_ex8_2015();
+    run_ex9_2015();
 }
